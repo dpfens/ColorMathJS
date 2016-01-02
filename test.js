b9b712be73a2e2a2f679b7b865f0a18f367564db
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-01-01 21:56:52 -0500

    moved tests to test.js file

---
+++ test.js
@@ -0,0 +1,14 @@
+test = new Color(150, 2, 255,0.2);
+test2 = new Color(150,50,50,0.8);
+test3 = new Color("aquamarine");
+test4 = new Color("#0ff");
+difference = test2.subtract(2,test);
+product = test.multiply(test2,2);
+quotient = test2.divide(5,test);
+log = test2.log(2,test);
+exp = test.pow(2);
+newColor = Color.prototype.alpha.call(test2,0.9);
+blend = test.blend( test2, 0.5 );
+gradient = Color.prototype.steps.call(test,test2, 4);
+
+console.log(gradient);
