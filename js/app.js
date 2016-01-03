8a76fd9cc5154e0b9100290babb9b5f871a4bd77
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2016-01-02 20:00:30 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+app = angular.module('app',[]);
+
+app.controller('visualController', function($scope) {
+	
+});
+app.controller('inputController', function($scope) {
+	
+});
+app.controller('outputController', function($scope) {
+	
+});
